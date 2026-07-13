eaa8af12860c282dbff5c968cd6c8e72376262c3
Skulldunno <fregennick@gmail.com>
2026-07-13 13:30:45 +0200

    changes to the GameClass roundloop resets available rerolls

--- GameClass.py
+++ GameClass.py
@@ -11,7 +11,7 @@ class Game:
         dice4 = D.Dice()
         dice5 = D.Dice()
         self.dice_list = (dice1, dice2, dice3, dice4, dice5)
-        self.scoresheet = scoshe.ScoreSheet("nick")
+        self.scoresheet = scoshe.ScoreSheet()
 
     def game_over_check(self): # this method checks if the game is over and all scoresheet fields are filled
         if None not in self.scoresheet.__dict__.values():
@@ -23,19 +23,16 @@ class Game:
             self.__rounds_left -= 1
             self._round_loop()
 
-    def _round_loop(self): # This method starts a round loop
-        #for i in range(self.__rerolls_left):
-        #   pass
-        # This will not be a standard loop we need to make it so the reroll button must be 
-        # pressed atleast once and can be pressed 2 additional times
-        # after rolling atleast once can the decision wether or not you want to fill in a field be made
-        # only after that does the round loop end
-        pass
+    def _round_loop(self): # This method starts a round loop, which is just a reset of rerolls and not an actual loop
+        # might also be useless as we are using pygame
+        self.__rerolls_left = 3
         
-    def roll_all_dice(self): # This method rolls every die or the locked die will not get rolled
-        for die in self.dice_list:
-            die.roll()
-    
+    def roll_all_dice(self): # This method rolls every die or the locked die will not get rolled can only be rolled a max of 3 times
+        if self.__rerolls_left < 0:
+            for die in self.dice_list:
+                die.roll()
+            self.__rerolls_left -=0
+
     def print_all_dice(self): # This method prints out the eyecount of every die for now only usefull for debugging
         for die in self.dice_list:
             print(die.get_eyes())
