20431506dceaa868a2f847a3e0488321ba37f39e
Fregen <FregenNick@srhk.de>
2026-07-14 10:44:32 +0200

    scorebonus will get calculated after every number input

--- DiceClass.py
+++ DiceClass.py
@@ -1,9 +1,9 @@
 import random
 
 class Dice:
-    def __init__(self, eyes = 1, locked = False):
-        self.__eyes = eyes
-        self.__locked = locked
+    def __init__(self):
+        self.__eyes = 1
+        self.__locked = False
 
     def roll(self): # This Method rolls the die and gives random number from 1 to 6
         if self.__locked == False:
